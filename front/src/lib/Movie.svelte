a8fb0dd61e7b6f04c012b5687044cbccf4c9d7d5
Felippe Scheidt <felippescheidt@gmail.com>
2024-12-14 00:19:08 -0300

    componente MovieDB consumir dados Atlas DB

--- front/src/lib/Movie.svelte
+++ front/src/lib/Movie.svelte
@@ -1,4 +1,5 @@
 <script>
+import { onMount } from "svelte";
 let results = $state(null);
 let debug = $state(false);
 async function getMovies() {
@@ -9,39 +10,40 @@ async function getMovies() {
            return data;
        } else {throw new Error(data); }
 }
-
-// condição de disparo da consulta (button onclick)
-function handleClick() {
+onMount(() => {
    getMovies().then((data)=>{
-       // gatilho para o mecanismo de reatividade
        results = data["results"]; 
    });
-}
+});
 </script>
-<h1>top 20 popular movies</h1>
-<button onclick={handleClick}>list</button>
 
-{#if results}
+<main>
+
+    <h1>top 20 popular movies</h1>
+    
+    {#if results}
     <input type="checkbox" bind:checked={debug}>
     {#if debug}
-        <pre>
-            {@html JSON.stringify(results[0], null, 2)}
-        </pre>
+    <pre>{JSON.stringify(results[0], null, 2)}</pre>
     {/if}
-     <div class="results">
-         {#each results as movie }
-            <p><img src="https://image.tmdb.org/t/p/w185/{movie.poster_path}" alt="{movie.title}"></p>
-            <div>
-                <h2>{ movie.title }</h2>
-                <p>{ movie.release_date}</p>
-            </div>
-         {/each}
+    <div class="results">
+        {#each results as movie }
+        <p><img src="https://image.tmdb.org/t/p/w185/{movie.poster_path}" alt="{movie.title}"></p>
+        <div>
+            <h2>{ movie.title }</h2>
+            <p>{ movie.release_date}</p>
+        </div>
+        {/each}
     </div>
-{/if}
-
+    {/if}
+</main>
+    
 <style>
+main{
+    padding: 10px;
+}
 pre{
-    white-space: pre-line;
+    white-space: pre-wrap;
 }
 .results{
     display:grid;
--- front/src/lib/MovieDB.svelte
+++ front/src/lib/MovieDB.svelte
@@ -13,38 +13,47 @@ async function getMovies() {
   }
 
 // condição de disparo da consulta 
-// (quando o componente foi carregado)
+// (quando o componente termina de ser carregado)
 onMount(() => {
     getMovies().then((data)=>{
         results = data["movies"]; 
     });
 });
 </script>
-<h1>Filmes favoritos</h1>
 
-{#if results}
+<main>
+
+    <h1>Filmes favoritos</h1>
+    
+    {#if results}
+    
     <input type="checkbox" bind:checked={debug}>
+    
     {#if debug}
-        <pre>
-            {@html JSON.stringify(results[0], null, 2)}
-        </pre>
+        <pre>{JSON.stringify(results[0], null, 2)}</pre>
     {/if}
-        <div class="results">
-            {#each results as movie }
-            {#if movie.poster_path }
-                <p><img src="https://image.tmdb.org/t/p/w185/{movie.poster_path}" alt="{movie.title}"></p>
-            {/if}
-            <div>
-                <h2>{ movie.title }</h2>
-                <p>{ movie.release_date}</p>
-            </div>
-            {/each}
+    
+    <div class="results">
+        {#each results as movie }
+        {#if movie.poster_path }
+        <p><img src="https://image.tmdb.org/t/p/w185/{movie.poster_path}" alt="{movie.title}"></p>
+        {/if}
+        <div>
+            <h2>{movie.title }</h2>
+            <p>{movie.release_date}</p>
+        </div>
+        {/each}
     </div>
-{/if}
+    {/if}
+    
+</main>
 
 <style>
+main{
+    padding: 10px;
+}
 pre{
-    white-space: pre-line;
+    white-space: pre-wrap;
 }
 .results{
     display:grid;
