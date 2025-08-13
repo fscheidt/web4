a88d170349ee8909559597a4fb72b92d2d01c2fb
Felippe Scheidt <felippescheidt@gmail.com>
2025-08-12 22:05:12 -0300

    Aula 2 - FastAPI

---
+++ main.py
@@ -0,0 +1,51 @@
+from fastapi import FastAPI
+app = FastAPI()
+
+data = {
+    55: "Brazil",
+    20: "Egito",
+    54: "Argentina"
+}
+
+# /country/55
+@app.get("/country/{area}")
+def country_area(area: int):
+    if area in data:
+        return {
+            "area": area,
+            "name": data[area]
+        }
+
+@app.get("/users")
+def list_users():
+    import json
+    with open("users.json") as f:
+        return json.load(f)
+
+
+# /sortear/6/60
+@app.get("/sortear/{quant}/{max}")
+def sortear():
+    # TODO :)
+    pass
+
+# DEFINIR O ENDPOINT
+# http://localhost:8000/
+
+@app.get("/")
+def hello():
+    return { "mensagem": "Hello World" }
+
+@app.get("/now")
+def datetime_now():
+    from datetime import datetime
+    return {
+        "datahora": datetime.now(),
+        "pais": "BR"
+    }
+
+
+# uvicorn main:app --reload
+
+
+
