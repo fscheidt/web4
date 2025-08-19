738f88ddd07da721a8ff0c0f727de9f6794db24e
Felippe Scheidt <felippescheidt@gmail.com>
2025-08-19 20:41:24 -0300

    Aula 3 - request (dog api)

---
+++ dogs.py
@@ -0,0 +1,14 @@
+import requests # pip install requests
+"""
+   ENDPOINT: /breeds/list/all
+"""
+url = "https://dog.ceo/api/breeds/list/all"
+
+# cabeçalho da requisição HTTP
+headers = { "accept": "application/json" }
+# realiza a requisição HTTP (GET) para o endpoint
+response = requests.get(url, headers=headers)
+# response é a resposta retornada pelo servidor
+print(response.json())
+
+# python3 dogs.py
