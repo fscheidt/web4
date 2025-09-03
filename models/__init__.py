0ea0ff49ca4b631504e1132f26852616b95ce78b
Felippe Scheidt <felippescheidt@gmail.com>
2025-09-02 21:42:31 -0300

    Aula 5 - Genre model

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, computed_field
 
 class User(BaseModel):
    id: int
@@ -7,14 +7,26 @@ class User(BaseModel):
    # campo opcional:
    avatar: str | None = None
 
+
 class Movie(BaseModel):
    id: int
+   title: str
    genre_ids: list | None = None
+   genres: list | None = None
    original_title: str
+   original_language: str | None = None
    overview: str
    popularity: float
    poster_path: str | None = None
    release_date: str
-   title: str
    vote_average: float
    vote_count: int
+
+
+class Genre(BaseModel):
+   id: int
+   name: str
+   @computed_field
+   @property
+   def label(self) -> str:
+      return self.name.replace(" ","-").lower()
